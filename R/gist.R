21650cfd05087028c8fe360897911be3662cd34c
Scott Chamberlain <myrmecocystus@gmail.com>
2014-05-27 11:07:49 -0700

    added delete and star fxns, and mssg fxn

--- R/gist.R
+++ R/gist.R
@@ -22,14 +22,14 @@
 #' @keywords internal
 #' @export
 
-gist <- function(gist, description = "", public = TRUE) {
+gist_post <- function(gist, description = "", public = TRUE) {
   dat <- create_gist(gist, description = description, public = public)
   credentials <- get_credentials()
-  response <- POST(url = "https://api.github.com/gists",
-                   body = dat, config = c(
-          authenticate(getOption("github.username"),
-              getOption("github.password"), type = "basic"), add_headers(`User-Agent` = "Dummy")))
-  stop_for_status(response)
+  headers <- add_headers(`User-Agent` = "Dummy", `Accept` = 'application/vnd.github.v3+json')
+  auth  <- authenticate(getOption("github.username"), getOption("github.password"), type = "basic")
+  response <- POST(url = "https://api.github.com/gists", body = dat, config = c(auth, headers))
+  warn_for_status(response)
+  assert_that(response$headers$`content-type` == 'application/json; charset=utf-8')
   html_url <- content(response)$html_url
   message("Your gist has been published")
   message("View gist at ", paste("https://gist.github.com/", getOption("github.username"),
@@ -49,7 +49,7 @@ gist <- function(gist, description = "", public = TRUE) {
 #' @keywords internal
 create_gist <- function(filenames, description = "", public = TRUE) {
   files <- lapply(filenames, function(file) {
-    x <- list(content = paste(readLines(file, warn = F), collapse = "\n"))
+    x <- list(content = paste(readLines(file, warn = FALSE), collapse = "\n"))
   })
   names(files) <- basename(filenames)
   body <- list(description = description, public = public, files = files)
@@ -68,3 +68,86 @@ get_credentials <- function() {
     options(github.password = password)
   }
 }
+
+gist_update <- function(gist, id, description = "", public = TRUE){
+  dat <- create_gist(gist, description = description, public = public)
+  credentials = get_credentials()
+  url <- sprintf('https://api.github.com/gists/%s', id)
+  headers <- add_headers(`User-Agent` = "Dummy", `Accept` = 'application/vnd.github.v3+json')
+  auth  <- authenticate(getOption("github.username"), getOption("github.password"), type = "basic")
+  response <- PATCH(url, body = gist, config = c(auth, headers))
+  warn_for_status(response)
+  assert_that(response$headers$`content-type` == 'application/json; charset=utf-8')
+  html_url = content(response)$html_url
+  message('Your gist has been updated')
+  invisible(basename(html_url))
+}
+
+#' Delete a gist
+#' 
+#' @param id Gist id
+#' @param verbose Print messages (default TRUE)
+#' @export
+#' @examples \dontrun{
+#' gist_delete(id='42095d9a661b0eefc694')
+#' }
+gist_delete <- function(id, verbose=TRUE){
+  credentials = get_credentials()
+  url <- sprintf('https://api.github.com/gists/%s', id)
+  headers <- add_headers(`User-Agent` = "Dummy", `Accept` = 'application/vnd.github.v3+json')
+  auth  <- authenticate(getOption("github.username"), getOption("github.password"), type = "basic")
+  response <- DELETE(url, config = c(auth, headers))
+  if(!response$status_code == 204){ message(response$headers$statusmessage) } else {
+    assert_that(response$headers$statusmessage == 'No Content')
+    mssg(verbose, 'Your gist has been deleted')
+  }
+}
+
+#' Star a gist
+#' 
+#' @param id Gist id
+#' @param verbose Print messages (default TRUE)
+#' @export
+#' @examples \dontrun{
+#' gist_star(id='2f9eb6b72730fed2f061')
+#' }
+gist_star <- function(id, verbose=TRUE){
+  credentials = get_credentials()
+  url <- sprintf('https://api.github.com/gists/%s/star', id)
+  headers <- add_headers(`User-Agent` = "Dummy", `Accept` = 'application/vnd.github.v3+json')
+  auth  <- authenticate(getOption("github.username"), getOption("github.password"), type = "basic")
+  response <- PUT(url, config = c(auth, headers))
+  if(!response$status_code == 204){ message(response$headers$statusmessage) } else {
+    assert_that(response$headers$statusmessage == 'No Content')
+    mssg(verbose, 'Your gist has been deleted')
+  }
+}
+
+#' Handler to print messages or not via verbose parameter in all fxns
+#' @keywords internal
+mssg <- function(x, y) if(x) message(y)
+
+# #' Response handler
+# #' @keywords internal
+# res_handler <- function(x){
+#   if(!x$status_code == 200){
+#     stnames <- names(content(x))
+#     if(!is.null(stnames)){
+#       if('developerMessage' %in% stnames|'message' %in% stnames){
+#         warning(sprintf("Error: (%s) - %s", x$status_code, 
+#                         noaa_compact(list(content(x)$developerMessage, content(x)$message))))
+#       } else { warning(sprintf("Error: (%s)", x$status_code)) }
+#     } else { warn_for_status(x) }
+#   } else {
+#     assert_that(x$headers$`content-type`=='application/json;charset=UTF-8')
+#     res <- content(x, as = 'text', encoding = "UTF-8")
+#     out <- jsonlite::fromJSON(res, simplifyVector = FALSE)
+#     if(!'results' %in% names(out)){
+#       if(length(out)==0){ warning("Sorry, no data found") }
+#     } else {
+#       if( class(try(out$results, silent=TRUE))=="try-error" | is.null(try(out$results, silent=TRUE)) )
+#         warning("Sorry, no data found")
+#     }
+#     return( out )
+#   }
+# }
