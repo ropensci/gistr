0610839077b0baf909b77b393dd85d02597dd5da
Scott Chamberlain <myrmecocystus@gmail.com>
2014-10-17 13:21:23 -0700

    finished test for gist() fxn

--- tests/testthat/test-gist.R
+++ tests/testthat/test-gist.R
@@ -3,10 +3,15 @@ context("gist")
 
 test_that("gist works", {
   expect_is(gist(id = 'f1403260eb92f5dfa7e1'), "gist")
-  expect_equal(length(gists(per_page=2)), 2)
+})
+
+test_that("as.gist works", {
+  expect_is(as.gist('f1403260eb92f5dfa7e1'), "gist")
+  expect_is(as.gist(10), "gist")
+  expect_is(as.gist(gist('f1403260eb92f5dfa7e1')), "gist")
 })
 
 test_that("config options work", {
   library('httr')
-  expect_error(gists(config=timeout(0.1)))
+  expect_error(gist(10, config=timeout(0.1)))
 })
