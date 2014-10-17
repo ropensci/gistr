08722a6f7c9b937a071d998418858d835ac4285f
Scott Chamberlain <myrmecocystus@gmail.com>
2014-10-17 11:30:08 -0700

    removed gist_update file, added missing params to gist_create

--- R/gist_create.R
+++ R/gist_create.R
@@ -4,6 +4,8 @@
 #' @param files Files to upload
 #' @param description (character) Brief description of gist (optional)
 #' @param public (logical) Whether gist is public (default: TRUE)
+#' @param browse (logical) To open newly create gist in default browser (default: TRUE)
+#' @param ... Further args passed on to \code{link[httr]{POST}}
 #' @examples \donttest{
 #' gist_create(files="~/stuff.md", description='a new cool gist')
 #' gist_create(files=c("~/spocc_sp.Rmd","~/spocc_sp.md"), description='spocc demo files')
