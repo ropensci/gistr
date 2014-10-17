483a985b3f96dab85b95827f3534cab7b7d2ed78
Scott Chamberlain <myrmecocystus@gmail.com>
2014-10-17 10:18:27 -0700

    added browse fxn

---
+++ R/browse.R
@@ -0,0 +1,11 @@
+#' Open a gist on GitHub
+#' 
+#' @param gist A gist object or something that can be coerced to a gist object.
+#' @param what One of html (default), json, forks, commits, or comments. 
+#' @export
+browse <- function(gist, what="html"){
+  gist <- as.gist(gist)
+  url <- switch(what, 
+         html="html_url", json="url", forks="forks_url", commits="commits_url", comments="comments_url")
+  browseURL(gist$data[[url]])
+}
