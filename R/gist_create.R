a1339d6b43db47ca323197c7be4c10be3229260e
Scott Chamberlain <myrmecocystus@gmail.com>
2014-10-20 11:55:46 -0700

    fixed missing optional browse param in gist_create

--- R/gist_create.R
+++ R/gist_create.R
@@ -16,6 +16,6 @@ gist_create <- function(files, description = "", public = TRUE, browse = TRUE, .
   body <- creategist(files, description, public)
   res <- gist_POST(gist_auth(), ghead(), body, ...)
   gist <- as.gist(res)
-  browse(gist)
+  if(browse) browse(gist)
   return( gist )
 }
