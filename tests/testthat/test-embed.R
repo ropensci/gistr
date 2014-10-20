31d4648e9d2a3e05aad07a987b4de2d685e15101
Scott Chamberlain <myrmecocystus@gmail.com>
2014-10-20 13:22:08 -0700

    fixed two tests

--- tests/testthat/test-embed.R
+++ tests/testthat/test-embed.R
@@ -1,7 +1,7 @@
-# tests for delete
-context("delete")
+# tests for embed
+context("embed")
 
-test_that("delete returns correct string and is character", {
+test_that("embed returns correct string and is character", {
   expect_output(gists()[[1]] %>% embed(), "script src=")
   expect_is(gists()[[1]] %>% embed(), "character")
 })
--- tests/testthat/test-fork.R
+++ tests/testthat/test-fork.R
@@ -5,14 +5,14 @@ test_that("fork works", {
   forked <- gist(id='1642874') %>% fork()
   
   expect_equal(length(forked), 16)
-  expect_is(length(forked), "gist")
+  expect_is(forked, "gist")
   
   # cleanup
   suppressMessages(forked %>% delete())
 })
 
 test_that("forks works", {
+  suppressPackageStartupMessages(library('magrittr'))
   expect_equal(length(gist(id='1642874') %>% forks(per_page=2)), 2)
   expect_is(gist(id='1642874') %>% forks(per_page=2) %>% extract2(length(.)), "gist")
 })
-
