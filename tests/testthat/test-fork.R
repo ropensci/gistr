0feb2be1733bfa43762d0f00a7a363ee2d1a1ed3
Scott Chamberlain <myrmecocystus@gmail.com>
2014-10-20 13:13:04 -0700

    added fork(s) test, #7

---
+++ tests/testthat/test-fork.R
@@ -0,0 +1,18 @@
+# tests for fork
+context("fork")
+
+test_that("fork works", {
+  forked <- gist(id='1642874') %>% fork()
+  
+  expect_equal(length(forked), 16)
+  expect_is(length(forked), "gist")
+  
+  # cleanup
+  suppressMessages(forked %>% delete())
+})
+
+test_that("forks works", {
+  expect_equal(length(gist(id='1642874') %>% forks(per_page=2)), 2)
+  expect_is(gist(id='1642874') %>% forks(per_page=2) %>% extract2(length(.)), "gist")
+})
+
