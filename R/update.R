0f32395984ec4e95a5a6c6c8a110659b355da81b
Scott Chamberlain <myrmecocystus@gmail.com>
2015-01-02 13:05:09 -0800

    changed edit to update, #18

--- R/update.R
+++ R/update.R
@@ -8,34 +8,34 @@
 #' # add new files
 #' gists(what = "minepublic")[[3]] %>%
 #'  add_files("~/zillowstuff.Rmd", "~/zoo.json") %>%
-#'  edit()
+#'  update()
 #'
-#' # edit existing files
+#' # update existing files
 #' ### file name has to match to current name
 #' gists(what = "minepublic")[[3]] %>%
-#'  edit_files("~/zillowstuff.Rmd") %>%
-#'  edit()
+#'  update_files("~/zillowstuff.Rmd") %>%
+#'  update()
 #'
 #' # delete existing files
 #' ### again, file name has to match to current name
 #' gists(what = "minepublic")[[3]] %>%
 #'  delete_files("~/zillowstuff.Rmd") %>%
-#'  edit()
+#'  update()
 #'
 #' # rename existing files
 #' # For some reason, this operation has to upload the content too
 #' ### first name is old file name with path (must match), and second is new file name (w/o path)
 #' gists(what = "minepublic")[[3]] %>%
 #'  rename_files(list("~/zillowstuff.Rmd", "zillow_pillow.Rmd")) %>%
-#'  edit()
+#'  update()
 #' ### you can pass in many renames
 #' rename_files(list("~/zillowstuff.Rmd", "zillow_pillow.Rmd"),
 #'              list("~/myfile.Rmd", "herfile.Rmd"))
 #' }
 
-edit <- function(gist, description = gist$description, ...)
+update <- function(gist, description = gist$description, ...)
 {
-  files <- list(edit=gist$edit_files, add=gist$add_files, delete=gist$delete_files, rename=gist$rename_files)
+  files <- list(update=gist$update_files, add=gist$add_files, delete=gist$delete_files, rename=gist$rename_files)
   body <- payload(filenames = files, description)
   res <- gist_PATCH(gist$id, gist_auth(), ghead(), body, ...)
   as.gist(res)
