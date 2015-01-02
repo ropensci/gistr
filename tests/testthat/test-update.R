42e8df58cdad57d3890c6194d5cba6f2479fc8a8
Scott Chamberlain <myrmecocystus@gmail.com>
2015-01-02 13:07:30 -0800

    changed edit test file name to update

--- tests/testthat/test-update.R
+++ tests/testthat/test-update.R
@@ -1,13 +1,13 @@
-# tests for edit
-context("edit")
+# tests for update
+context("update")
 
-test_that("edit xxxx", {
-  # crate a new gist first, then edit it
+test_that("update works", {
+  # crate a new gist first, then update it
   stuff <- system.file("examples", "stuff.md", package = "gistr")
   g <- gist_create(stuff, "gist gist gist", TRUE, browse = FALSE)
   expect_equal(names(g$files), "stuff.md")
   
   zoo <- system.file("examples", "zoo.json", package = "gistr")
-  ed <- g %>% add_files(zoo) %>% edit()
+  ed <- g %>% add_files(zoo) %>% update()
   expect_equal(names(ed$files), c("stuff.md","zoo.json"))
 })
