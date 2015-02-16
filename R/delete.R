d7a187517b1229e54e5ae0c627aa725990d3caeb
Scott Chamberlain <myrmecocystus@gmail.com>
2015-02-16 15:37:35 -0800

    new fxn tbl() to coerce gist class or list of gist class objects to df #25 also changed delete fxn to act on gist class or tbl class as eg of various gist actions on data.frames

--- R/delete.R
+++ R/delete.R
@@ -1,16 +1,29 @@
 #' Delete a gist
 #'
-#' @param gist A gist object or something coerceable to a gist
-#' @template all
 #' @export
+#' @param x A gist object or something coerceable to a gist
+#' @template all
+#' 
 #' @examples \dontrun{
 #' gists("minepublic")[[29]] %>% delete()
 #' }
+delete <- function(x, ...) UseMethod("delete")
 
-delete <- function(gist, ...)
+#' @export
+#' @rdname delete
+delete.gist <- function(x, ...)
 {
-  gist <- as.gist(gist)
+  gist <- as.gist(x)
   res <- gist_DELETE(paste0(ghbase(), '/gists/', gist$id), auth = gist_auth(), headers = ghead(), ...)
   stop_for_status(res)
   message('Your gist has been deleted')
+}
+
+#' @export
+#' @rdname delete
+delete.tbl <- function(x, ...) invisible(lapply(x$id, delgist, ...))
+
+delgist <- function(z, ...){
+  res <- gist_DELETE(paste0(ghbase(), '/gists/', z), auth = gist_auth(), headers = ghead(), ...)
+  warn_for_status(res)
 }
