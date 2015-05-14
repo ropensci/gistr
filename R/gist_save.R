39d816688c02fdf5bd872f70ed3c6eae854f4b4f
Scott Chamberlain <myrmecocystus@gmail.com>
2015-05-14 15:26:49 -0700

    gist_open split out from gist_save to open files separately fix #49, bumped 99 ver

--- R/gist_save.R
+++ R/gist_save.R
@@ -1,20 +1,30 @@
-#' Save a gist to disk
+#' Save gist files to disk
 #'
 #' @export
 #' @param gist A gist object or something coerceable to a gist
-#' @param path Path to write to, a directory, not a file b/c a gist can contain 
-#' many files. File names will be the same as given in the gist
-#' @param open_files (logical) If \code{TRUE}, open files in your editor/R GUI. 
-#' Default: \code{FALSE}
-#' @return file(s) path
+#' @param path Root path to write to, a directory, not a file b/c a gist can contain 
+#' many files. A folder is created with name of the gist id within this root 
+#' directory.  File names will be the same as given in the gist.
+#' @param x An object of class \code{gist_files} (the output from \code{gist_save})
+#' @return An object of class \code{gist_files}, S3 object containing file paths
+#' @details 
+#' \code{gist_save}: files are written into a new folder, named by the gist id, 
+#' e.g., \code{a65ac7e56b7b3f746913}
+#' 
+#' \code{gist_open}: opens files in your editor/R GUI. Internally, uses 
+#' \code{\link{file.edit}} to open files, using \code{getOption("editor")} to 
+#' open the files. If you're in R.app or RStudio, or other IDE's, files will 
+#' open in the IDE (I think).
 #' @examples \dontrun{
 #' gist("a65ac7e56b7b3f746913") %>% gist_save()
+#' gist("a65ac7e56b7b3f746913") %>% gist_save() %>% gist_open()
 #' gist("https://gist.github.com/expersso/4ac33b9c00751fddc7f8") %>% gist_save()
 #' }
 
-gist_save <- function(gist, path = ".", open_files = FALSE) {
+gist_save <- function(gist, path = ".") {
   gist <- as.gist(gist)
-  path <- path.expand(path)
+  if (!isDir(path)) stop("path does not exist: ", path, call. = FALSE)
+  path <- file.path(path.expand(path), gist$id)
   dir.create(path, showWarnings = FALSE, recursive = TRUE)
   if (is.null(gist$files) || length(gist$files) == 0) {
     stop("no files to write", call. = FALSE)
@@ -25,8 +35,35 @@ gist_save <- function(gist, path = ".", open_files = FALSE) {
     files[[i]] <- writepath <- file.path(path, x$filename)
     writeLines(x$content, writepath)
   }
-  if (open_files) {
-    invisible(lapply(files, file.edit))
-  }
-  return(unlist(files))
+  structure(unlist(files), class = "gist_files", id = gist$id)
+}
+
+#' @export
+print.gist_files <- function(x, ...) {
+  cat("<gist> ", attr(x, "id"), "\n", sep = "")
+  # cat("  Files: \n", paste0(strx, collapse = "\n"), "\n", sep = "")
+  cat(g_wrap(sprintf("Files:\n %s ...", paste0(x, collapse = "\n")), indent = 3), "\n\n")
+}
+
+g_wrap <- function(..., indent = 0, width = getOption("width")) {
+  x <- paste0(..., collapse = "")
+  wrapped <- strwrap(x, indent = indent, exdent = indent + 2, width = width)
+  paste0(wrapped, collapse = "\n")
+}
+
+#' @export
+#' @rdname gist_save
+gist_open <- function(x) {
+  stopifnot(is(x, "gist_files"))
+  invisible(lapply(x, file.edit))
+}
+
+isDir <- function(path) {
+  if (!is.string(path)) stop("path is not a string: ", path, call. = FALSE)
+  if (!file.exists(path)) stop("path does not exist: ", path, call. = FALSE)
+  file.info(path)$isdir
+}
+
+is.string <- function(x) { 
+  is.character(x) && length(x) == 1
 }
