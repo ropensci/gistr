b7b0ca75af3f8ab35ed09b5d18630e55164e315c
Scott Chamberlain <myrmecocystus@gmail.com>
2015-07-02 16:05:59 -0700

    fixed test for gist_save

--- tests/testthat/test-gist_save.R
+++ tests/testthat/test-gist_save.R
@@ -1,7 +1,11 @@
 context("gist_save")
 
 test_that("gist_save works", {
-  aa <- gist("a65ac7e56b7b3f746913") %>% gist_save()
-  expect_is(aa, "character")
+  dir <- tempdir()
+  aa <- gist("a65ac7e56b7b3f746913") %>% gist_save(dir)
+  expect_is(aa, "gist_files")
   expect_is(aa[[1]], "character")
+  
+  # cleanup
+  unlink(dir, recursive = TRUE, force = TRUE)
 })
