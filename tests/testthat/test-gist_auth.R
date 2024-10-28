e0ce73170d3c7112358dcac6a79cac6fdd44ebd1
Eric Scott <scottericr@gmail.com>
2024-10-28 11:32:41 -0700

    add some tests for auth

---
+++ tests/testthat/test-gist_auth.R
@@ -0,0 +1,16 @@
+test_that("gist_auth finds PAT", {
+  skip_on_cran()
+  auth <- gist_auth(reauth = TRUE)
+  expect_equal(
+    auth$Authorization,
+    paste0("token ", gitcreds::gitcreds_get()$password)
+  )
+})
+
+#can't (easily) test PAT stored with gitcreds::gitcreds_set() or interactive oauth
+
+test_that("valid_gh_pat() works", {
+  expect_false(valid_gh_pat("hello"))
+  expect_false(valid_gh_pat(""))
+  expect_false(valid_gh_pat(NULL))
+})
